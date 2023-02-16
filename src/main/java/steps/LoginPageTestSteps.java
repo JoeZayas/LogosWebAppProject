67c7b3d4510d96727c6a703452fe6857ed0f4dbd
joezayas <joedza4610@gmail.com>
2023-02-16 15:10:38 -0600

    1st commit

---
+++ src/main/java/steps/LoginPageTestSteps.java
@@ -0,0 +1,44 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageTestSteps {
+    @Given("user is navigated to logos bible application")
+    public void user_is_navigated_to_logos_bible_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks top right sign in button")
+    public void user_clicks_top_right_sign_in_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("authentication pop-up appears")
+    public void authentication_pop_up_appears() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enters valid username and password")
+    public void user_enters_valid_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks on pop up sign in button")
+    public void user_clicks_on_pop_up_sign_in_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user is logged into application")
+    public void user_is_logged_into_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
